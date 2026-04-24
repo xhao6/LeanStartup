9a2d6f95a3aeac486afb0b64e0a53ef876be4aad
Ava Bytewood <xhaoca@foxmail.com>
2026-04-24 09:35:20 +0800

    chore: remove frontend code
    Delete all uni-app frontend files including:
    - Root config files (package.json, tsconfig, vite, etc.)
    - src/ directory with all components, pages, composables
    - dist/, styles/ build artifacts

--- src/config/app.config.ts
+++
@@ -1,9 +0,0 @@
-const appConfig = {
-  appName: '精益副业案例库',
-  appDesc: '每日精选3个高价值副业案例',
-  version: '1.0.0',
-  contactEmail: 'xhaoca@foxmail.com',
-  copyrightYear: 2026
-}
-
-export default appConfig
--- src/store/index.ts
+++
@@ -1,4 +0,0 @@
-export { useUserStore } from './user'
-export { useCollectionStore } from './collection'
-export type { CollectionItem } from './collection'
-export { useSubscriptionStore } from './subscription'
