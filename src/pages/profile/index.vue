0287307f79527726f2f39714844f9e06bb6ee62e
Ava Bytewood <xhaoca@foxmail.com>
2026-04-27 23:17:13 +0800

    fix: ensure switchTab usage for tabbar page navigation

--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -169,7 +169,7 @@ const handleLogout = () => {
 }
 
 const goToFavorites = () => {
-  uni.switchTab({ url: '/pages/profile/favorites/index' })
+  uni.navigateTo({ url: '/pages/profile/favorites/index' })
 }
 
 const goToSubscription = () => {
