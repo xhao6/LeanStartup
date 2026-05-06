1921b789e182b2a18d6e91ad1cd8c7e7554babdd
Ava Bytewood <xhaoca@foxmail.com>
2026-05-06 10:18:38 +0800

    feat(content-discoverer): add config with Sogou selectors

---
+++ scripts/content-discoverer/src/config.ts
@@ -0,0 +1,31 @@
+import path from "node:path";
+import type { DiscoverConfig } from "./types.js";
+
+export const DEFAULT_CONFIG: DiscoverConfig = {
+  accounts: [
+    // 在此添加目标公众号名称
+  ],
+  keywords: [
+    // 在此添加搜索关键词
+  ],
+  maxPages: 5,
+  batchSize: 10,
+  scanDelayMs: [3000, 5000],
+  sogouSearchUrl: "https://weixin.sogou.com",
+  outputDir: path.resolve(import.meta.dirname ?? ".", "../../../resources"),
+  selectors: {
+    searchBox: "#query",
+    searchButton: ".swz2",
+    searchTypeAccount: "a[href*='type=1']",
+    searchTypeArticle: "a[href*='type=2']",
+    accountResult: ".txt-box h3 a",
+    articleItem: ".news-list li",
+    articleTitle: ".txt-box h3 a",
+    articleExcerpt: ".txt-box p",
+    articleDate: ".s-p .s2",
+    articleUrl: ".txt-box h3 a[href]",
+    loadMoreButton: ".load-more",
+    captchaIndicator: "#captcha, .antispider",
+    timeSortTab: "a[href*='sort=time']",
+  },
+};
